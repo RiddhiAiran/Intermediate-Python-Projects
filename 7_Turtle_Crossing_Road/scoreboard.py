26a0c3374c65648ca6285f5de96e03e6a3686d9c
Riddhi <datascience.riddhi@gmail.com>
2025-01-05 05:54:49 +0530

    udpated files

---
+++ 7_Turtle_Crossing_Road/scoreboard.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.level = 0
+        self.update_scoreboard()
+    
+    
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-250,270)
+        self.write(f'Level:{self.level}',align='center',font=('Arial',24,'bold'))
+        
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+    
